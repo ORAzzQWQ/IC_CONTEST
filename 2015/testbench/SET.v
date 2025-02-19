1e7d80e36ebe364076eb808b900f6b7f97468c4d
Chelsea <annie901010@gmail.com>
2025-02-19 23:34:09 +0800

    pre-sim all pass

--- 2015/testbench/SET.v
+++ 2015/testbench/SET.v
@@ -12,10 +12,10 @@ output reg [7:0] candidate;
 reg [3:0] x1, y1, x2, y2, r1, r2;
 reg [1:0] mode_reg;
 reg [3:0] i, j, x_min_A, y_min_A, x_max_A, y_max_A, x_min_B, y_min_B, x_max_B, y_max_B;
+reg [3:0] x_min_AB, y_min_AB, x_max_AB, y_max_AB, x_min_AB_2, y_min_AB_2, x_max_AB_2, y_max_AB_2;
 reg [6:0] cnt;
-reg [2:0] cal_cnt;
 reg [2:0] cal_done;
-reg [3:0] i_bar, j_bar;
+reg [3:0] i_bar, j_bar, i_bar_B, j_bar_B;
 
 
 reg [1:0] state, next_state;
@@ -67,10 +67,8 @@ always @(posedge clk or posedge rst) begin
         j <= 0;
         busy <= 0;
         cnt <= 0;
-        cal_cnt <= 0;
         cal_done <= 0;
         valid <= 0;
-        cnt <= 0;
     end
     else begin
         state <= next_state;
@@ -86,8 +84,21 @@ always @(posedge clk or posedge rst) begin
                 cal_done <= 0;
             end
             CAL_p:begin
-                i <= x_min_A;
-                j <= y_min_A;
+                case(mode_reg)
+                    2'b00:begin
+                        i <= x_min_A;
+                        j <= y_min_A;
+                    end
+                    2'b01:begin
+                        i <= x_min_AB;
+                        j <= y_min_AB;
+                    end
+                    2'b10:begin
+                        i <= x_min_AB_2;
+                        j <= y_min_AB_2;
+                    end
+                endcase
+
             end
             CAL:begin
                 busy <= 1;
@@ -109,8 +120,46 @@ always @(posedge clk or posedge rst) begin
                         end
                     end
                     2'b01:begin
+                        if(j <= y_max_AB) begin
+                            if(i <= x_max_AB) begin
+                                if(table_S[i_bar] + table_S[j_bar] <= table_S[r1] && 
+                                   table_S[i_bar_B] + table_S[j_bar_B] <= table_S[r2] ) cnt <= cnt + 1;
+                                i <= i+1;
+                            end
+                            else begin
+                                i <= x_min_AB;
+                                j <= j+1;
+                            end
+                        end
+                        else begin
+                            cal_done <= 1;
+                            valid <= 1;
+                        end
                     end
                     2'b10:begin
+                        if(j <= y_max_AB_2) begin
+                            if(i <= x_max_AB_2) begin
+                                if(table_S[i_bar] + table_S[j_bar] <= table_S[r1] || table_S[i_bar_B] + table_S[j_bar_B] <= table_S[r2] ) begin
+                                    if(table_S[i_bar] + table_S[j_bar] <= table_S[r1] && table_S[i_bar_B] + table_S[j_bar_B] <= table_S[r2]) begin
+                                    end
+                                    else begin
+                                        cnt <= cnt + 1;
+                                    end
+                                    // if (!(table_S[i_bar] + table_S[j_bar] <= table_S[r1] && table_S[i_bar_B] + table_S[j_bar_B] <= table_S[r2])) begin
+                                    //     cnt <= cnt + 1;
+                                    // end
+                                end
+                                i <= i+1;
+                            end
+                            else begin
+                                i <= x_min_AB_2;
+                                j <= j+1;
+                            end
+                        end
+                        else begin
+                            cal_done <= 1;
+                            valid <= 1;
+                        end
                     end
                 endcase
             end
@@ -130,13 +179,26 @@ always@(*) begin
     x_max_A = x1 + r1 >= 8 ? 8 : x1 + r1;
     y_max_A = y1 + r1 >= 8 ? 8 : y1 + r1;
 
-    x_min_B = x2 - r2 >= 1 ? x2 - r2 : 1;
-    y_min_B = y2 - r2 >= 1 ? y2 - r2 : 1;
+    x_min_B = x2 >= 1 + r2 ? x2 - r2 : 1;
+    y_min_B = y2 >= 1 + r2 ? y2 - r2 : 1;
     x_max_B = x2 + r2 >= 8 ? 8 : x2 + r2;
     y_max_B = y2 + r2 >= 8 ? 8 : y2 + r2;
 
+    x_min_AB = x_min_A < x_min_B ? x_min_B : x_min_A;
+    x_max_AB = x_max_A < x_max_B ? x_max_A : x_max_B;
+    y_min_AB = y_min_A < y_min_B ? y_min_B : y_min_A;
+    y_max_AB = y_max_A < y_max_B ? y_max_A : y_max_B;
+
+    x_min_AB_2 = x_min_A < x_min_B ? x_min_A : x_min_B;
+    x_max_AB_2 = x_max_A < x_max_B ? x_max_B : x_max_A;
+    y_min_AB_2 = y_min_A < y_min_B ? y_min_A : y_min_B;
+    y_max_AB_2 = y_max_A < y_max_B ? y_max_B : y_max_A;
+
     i_bar = i >= x1 ? i-x1 : x1-i;
     j_bar = j >= y1 ? j-y1 : y1-j;
+
+    i_bar_B = i >= x2 ? i-x2 : x2-i;
+    j_bar_B = j >= y2 ? j-y2 : y2-j;
     
     candidate = cnt;
 end
